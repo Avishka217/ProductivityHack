231909bf7bde6edec5ff08ce179f0fde226e8842
Avishka217 <avishka.sidath99@gmail.com>
2024-04-02 07:43:44 +0530

    title change

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ $conn->close();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Points Tracker</title>
+  <title>ProductivityHack</title>
   <!-- Bootstrap CSS -->
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
