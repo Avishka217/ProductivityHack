59d583d8ef8404b271b3a93e1e31348321b8a8bd
Avishka217 <avishka.sidath99@gmail.com>
2024-04-02 11:06:13 +0530

    style changes

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
 $currentPage = basename($_SERVER['PHP_SELF']);
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Points Tracker | Unless you try , you never know</a>
+  <a class="navbar-brand" href="#">Productivity | <span style="color:yellow;">Hack</span></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
